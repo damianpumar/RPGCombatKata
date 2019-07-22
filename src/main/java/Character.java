335187cae1cd4b7e459f80c9a72bc851b9cc3063
Damián Pumar <damianpumar@gmail.com>
2019-07-22 11:26:54 +0200

    Start health 1000 Test Green

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,12 @@
 public class Character {
+    private final int START_HEALTH = 1000;
+    private final int health;
+
+    public Character() {
+        this.health = 1000;
+    }
+
     public int health() {
-        throw new UnsupportedOperationException();
+        return this.health;
     }
 }
