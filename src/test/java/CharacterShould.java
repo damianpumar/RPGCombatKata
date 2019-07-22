dec5b0e81d5842e4161c1377f486c5a108596067
Gogs <gogs@fake.local>
2019-07-22 11:52:57 +0100

    Refactor to use a factory method for a damaged chracter

--- src/test/java/CharacterShould.java
+++ src/test/java/CharacterShould.java
@@ -1,72 +1,57 @@
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CharacterShould {
-    private Character character;
-
-    @Before
-    public void setup() {
-        this.character = new Character();
-    }
 
     @Test
     public void start_health_1000() {
+        Character character = new Character();
         assertThat(character.health()).isEqualTo(1000);
     }
 
     @Test
     public void start_level_is_1() {
+        Character character = new Character();
         assertThat(character.level()).isEqualTo(1);
     }
 
     @Test
     public void start_alive() {
+        Character character = new Character();
         assertThat(character.isAlive()).isTrue();
     }
 
     @Test
     public void deal_damage_to_other_character() {
-        Character otherCharacter = new Character();
-
-        character.dealDamage(otherCharacter, 400);
-
+        Character otherCharacter = getCharacterWithDamageOf(400);
         assertThat(otherCharacter.health()).isEqualTo(600);
     }
 
     @Test
     public void not_deal_damage_to_itself(){
+        Character character = new Character();
         character.dealDamage(character, 400);
-
         assertThat(character.health()).isEqualTo(1000);
     }
 
-
-
-
     @Test
     public void die_when_health_is_zero() {
-        Character otherCharacter = new Character();
-
-        character.dealDamage(otherCharacter, 1001);
-
+        Character otherCharacter = getCharacterWithDamageOf(1001);
         assertThat(otherCharacter.isAlive()).isFalse();
     }
 
     @Test
     public void have_health_zero_when_damage_received_exceeds_current_health() {
-        Character otherCharacter = new Character();
-
-        character.dealDamage(otherCharacter, 1001);
-
+        Character otherCharacter = getCharacterWithDamageOf(1001);
         assertThat(otherCharacter.health()).isEqualTo(0);
     }
 
     @Test
     public void heal_other_character() {
-        Character otherCharacter = new Character();
-        character.dealDamage(otherCharacter, 100);
+        Character character = new Character();
+
+        Character otherCharacter = getCharacterWithDamageOf(100);
 
         character.heal(otherCharacter, 50);
 
@@ -75,8 +60,9 @@ public class CharacterShould {
 
     @Test
     public void not_heal_character_when_is_dead() {
-        Character otherCharacter = new Character();
-        character.dealDamage(otherCharacter, 1001);
+        Character character = new Character();
+
+        Character otherCharacter = getCharacterWithDamageOf(1001);
 
         character.heal(otherCharacter, 50);
 
@@ -85,10 +71,19 @@ public class CharacterShould {
 
     @Test
     public void not_heal_above_1000() {
+        Character character = new Character();
         Character otherCharacter = new Character();
 
         character.heal(otherCharacter, 50);
 
         assertThat(otherCharacter.health()).isEqualTo(1000);
+    }
+
+    private Character getCharacterWithDamageOf(int damage) {
+        Character newCharacter = new Character();
+        Character damagingCharacter = new Character();
+
+        damagingCharacter.dealDamage(newCharacter, damage);
+        return newCharacter;
     }
 }
