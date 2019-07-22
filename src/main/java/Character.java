75e8a11bc7f02a531701ccf23b775a4044c05fec
Damián Pumar <damianpumar@gmail.com>
2019-07-22 11:33:01 +0200

    Start level is 1

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,12 +1,18 @@
 public class Character {
     private final int START_HEALTH = 1000;
+    private final int START_LEVEL = 1;
+
     private final int health;
 
     public Character() {
-        this.health = 1000;
+        this.health = START_HEALTH;
     }
 
     public int health() {
         return this.health;
+    }
+
+    public int level() {
+        return START_LEVEL;
     }
 }
--- src/test/java/CharacterShould.java
+++ src/test/java/CharacterShould.java
@@ -1,12 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CharacterShould {
+    private Character character ;
+
+    @Before
+    public void setup() {
+      this.character = new Character();
+    }
+
     @Test
     public void start_health_1000() {
-        Character character = new Character();
-
         assertThat(character.health()).isEqualTo(1000);
+    }
+
+    @Test
+    public void start_level_is_1() {
+        assertThat(character.level()).isEqualTo(1);
     }
 }
